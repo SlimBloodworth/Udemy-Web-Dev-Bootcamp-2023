10bc0a35df49177583469880fa397c7bd2c5aa09
Slim <83024708+SlimBloodworth@users.noreply.github.com>
2023-07-22 14:26:48 -0400

    Add notes for lessons #166 and #167 and add practice function to ls-exercises.js

--- app/js-exercises.js
+++ app/js-exercises.js
@@ -1,1 +1,11 @@
 /* js-exercises.html page */
+/*console.log("Hello World!"); - lesson #167*/
+/*Personal Practice Hello User*/
+function greetUser() {
+    let userName = document.getElementById("userName").value;
+    let greeting = "Hello " + userName + "!";
+    let greetingOutPut = document.getElementById("outPut");
+    greetingOutPut.value = greeting;
+}
+const submitButton = document.getElementById("jsUserSubmitBtn");
+submitButton.addEventListener("click", greetUser);
